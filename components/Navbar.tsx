97801a4cb30daf1bb651c0bedf6786eebba0c47b
Saba N <sabanvd34@gmail.com>
2025-03-09 01:38:56 +0500

    logo all messed up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -71,7 +71,7 @@ export default function Navbar({ items }: NavbarProps) {
       <Content>
         <NextLink href="/" passHref>
           <LogoWrapper>
-          <NextImage src={mainLogo} width={200} height={200} alt="lastcript"/> 
+          <NextImage src={mainLogo} width={110} height={120} alt="lastcript"/> 
           </LogoWrapper>
         </NextLink>
         <NavItemList>
